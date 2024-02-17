d4674e539232f8882fc041781a59bf2733a0f459
Alexander Görtzen <40467337+AlexanderG2207@users.noreply.github.com>
2024-02-17 20:11:08 +0100

    add settings.bundle and remove settings view

--- Apollon/Models/ApollonDiagram.swift
+++ Apollon/Models/ApollonDiagram.swift
@@ -18,11 +18,3 @@ import ApollonShared
         self.model = model ?? ""
     }
 }
-
-extension Date {
-    func ISO8601FormatWithFractionalSeconds() -> String {
-        let formatter = DateFormatter()
-        formatter.dateFormat = "yyyy-MM-dd'T'HH:mm:ss.SSSZ"
-        return formatter.string(from: self)
-    }
-}
